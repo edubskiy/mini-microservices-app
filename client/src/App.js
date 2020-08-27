433c38fd8fbe154ef3a60ff3be45ca152c6f71c1
Evgeniy Dubskiy <edubskiy@openwaygroup.com>
2020-08-28 00:54:20 +0300

    Added PostCreate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import PostCreate from './PostCreate';
 
 export default () => {
-  return <div>Blog App</div>;
+  return (
+    <div className="container">
+      <h1>Post Create</h1>
+      <br />
+      <PostCreate />
+    </div>
+  );
 }
---
+++ client/src/PostCreate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+  return <div>
+    <form>
+      <div className="div-group">
+        <label>Title</label>
+        <input className="form-control" />
+      </div>
+      <br />
+      <button className="btn btn-primary">Submit</button>
+    </form>
+
+  </div>
+}
